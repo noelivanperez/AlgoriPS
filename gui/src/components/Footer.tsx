99352f48382da8fbbe49d2bb67af51e2fc6df7a2
noelivanperez <noelivanperez82@gmail.com>
2025-07-16 15:50:05 -0800

    feat(gui): add footer

---
+++ gui/src/components/Footer.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Footer: React.FC = () => (
+  <footer className="footer">noel perez salda;a</footer>
+);
+
+export default Footer;
--- gui/src/layouts/Layout.tsx
+++ gui/src/layouts/Layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Sidebar from '../components/Sidebar';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const Layout: React.FC<React.PropsWithChildren> = ({ children }) => (
   <div className="app-layout">
@@ -8,6 +9,7 @@ const Layout: React.FC<React.PropsWithChildren> = ({ children }) => (
     <main className="app-main">
       <Header />
       {children}
+      <Footer />
     </main>
   </div>
 );
